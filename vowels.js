98e878cc2a299b05a70462f256b78dd576ba8a79
shailesh2137 <55477328+shailesh2137@users.noreply.github.com>
2022-09-15 09:03:50 +0530

    Update vowels.js

--- vowels.js
+++ vowels.js
@@ -1,3 +1,16 @@
-let str="aman";
-let N=4;
-//check this string vowel or not//
+let str = "aman";
+
+let obj = {} ;
+
+for( let i=0; i<=str.length-1; i++ )
+  {
+    if(obj[str[i]] == undefined)
+    {
+      obj[str[i]] = 1;
+    }
+    else
+    {
+      obj[str[i]]++;
+    }
+  }
+console.log(obj);
